bcdedbbf0022f40462795d9ea4ba5b3554c1f61e
Erik Sherman <erikkennethsherman@gmail.com>
2021-06-16 07:32:31 -0400

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+const HomePage = () => {
+    return (
+        <Container>
+            <h1>something</h1>
+        </Container>
+    )
+}
+
+export default HomePage
